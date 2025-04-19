ccdee5aee676b602fe7e4e2bac08c340408e1240
NMH-HowToCode <naingminhtet.dmy97@gmail.com>
2025-04-19 19:02:23 +0700

    Export Module

---
+++ module1.js
@@ -0,0 +1,9 @@
+const myname = "NaingMinHtet";
+const myage = 27;
+const myaddr = "No.4 Main Road M.O.G.E"
+
+module.exports = {
+    myname,
+    myage,
+    myaddr
+};
---
+++ module2.js
@@ -0,0 +1,3 @@
+const {myname, myage, myaddr} = require('./module1');
+
+console.log(myname, myage, myaddr);
